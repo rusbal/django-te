561a90e525cfb5a4b90442a4bd8778f1649c86b2
Raymond Usbal <raymond@philippinedev.com>
2013-08-23 05:02:45 +0800

    setting db

--- te/te/settings/base.py
+++ te/te/settings/base.py
@@ -9,18 +9,6 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
-    }
-}
-
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = []
@@ -113,6 +101,10 @@ TEMPLATE_DIRS = (
     # Don't forget to use absolute paths, not relative paths.
 )
 
+MY_APPS = (
+    # 'django_mailbox',
+)
+
 INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -120,11 +112,9 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
-)
+    'django.contrib.admin',
+    'south',
+) + MY_APPS
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
--- te/te/settings/local.py
+++ te/te/settings/local.py
@@ -1,1 +1,12 @@
 from .base import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'act_te',
+        'USER': 'postgres',
+        'PASSWORD': 'root1',
+        'HOST': '',
+        'PORT': '',
+    }
+}
