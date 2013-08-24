d961c1e09adbfd85466defc6767f936af7254b7e
Raymond Usbal <raymond@philippinedev.com>
2013-08-25 05:05:00 +0800

    verbose test

--- te/coverage.sh
+++ te/coverage.sh
@@ -1,3 +1,3 @@
 rm -rf app_data/test_coverage_report
-coverage erase; coverage run manage.py test; coverage html
+coverage erase; coverage run manage.py test -v 2; coverage html
 open app_data/test_coverage_report/index.html
