213fab7d49a903f440ac26494232a0ff17a62eea
Raymond Usbal <raymond@philippinedev.com>
2013-08-28 02:19:01 +0800

    updated homepage test

--- te/tests/dashboard/test_homepage.py
+++ te/tests/dashboard/test_homepage.py
@@ -14,5 +14,9 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        expected_html = render_to_string('index.html')
+        ctx = {
+            'js': 'index.js.html',
+            'css': 'index.css.html',
+        }
+        expected_html = render_to_string('index.html', ctx)
         self.assertEqual(response.content.decode(), expected_html)
